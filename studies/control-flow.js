86b0d30142702fd7efe32012e98bf8524c2ce1e8
EC2 Default User <ec2-user@ip-172-31-82-192.ec2.internal>
2019-08-12 02:53:55 +0000

    update studies

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -22,14 +22,15 @@
 let num = 4;
 12
 13
-if(num > 3) {
-14
+if (num > 3) {
+    14
     console.log(num);
-15
-} else {
-16
+    15
+}
+else {
+    16
     console.log(" num is greater than 3");
-17
+    17
 }
 18
 // prints: the num is greater than 3. 
@@ -46,18 +47,20 @@ the truthy condition or it runs the default statement
 let c = 6;
 26
 27
-if(c < 3) {
-28
+if (c < 3) {
+    28
     console.log("hi");
-29
-} else if(c <= 5) { // it runs the code because the condition is true
-30
+    29
+}
+else if (c <= 5) { // it runs the code because the condition is true
+    30
     console.log("how is your day going!");
-31
-} else{ // this is my default statement the other conditions are true
-32
+    31
+}
+else { // this is my default statement the other conditions are true
+    32
     console.log("Goodnight!");
-33
+    33
 }
 34
 // prints: Goodnight! 
@@ -78,35 +81,35 @@ let f = "get money";
 43
 44
 switch (f) {
-45
-    
-46
+    45
+
+    46
     case "crushed up":
-47
+        47
         console.log("its a cool song but its not my favorite song");
-48
+        48
         break;
-49
-        
-50
-        case "first song":
-51
-            console.log("I dont like that song");
-52
-            break;
-53
-            
-54
-        case "get money": // this case matches the expression so it would print the following statement
-55
-            console.log("I love that song, Can you play that for me?");
-56
-            break;
-57
-            
-58
-        default: 
-59
-            console.log("let me choose my favorite song");
-60
+        49
+
+        50
+    case "first song":
+        51
+        console.log("I dont like that song");
+        52
+        break;
+        53
+
+        54
+    case "get money": // this case matches the expression so it would print the following statement
+        55
+        console.log("I love that song, Can you play that for me?");
+        56
+        break;
+        57
+
+        58
+    default:
+        59
+        console.log("let me choose my favorite song");
+        60
 }
